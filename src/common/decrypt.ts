08ae0df55a63d85abb34cab05c995fb75f759a51
dattran <dattran.hn0802@gmail.com>
2023-04-19 16:01:40 +0700

    update encrypt flow

--- src/common/decrypt.ts
+++ src/common/decrypt.ts
@@ -2,7 +2,8 @@ var CryptoJS = require('crypto-js')
 
 export default class decrypt {
 	public static AES(toDecrypt: string, publicKey: string): string {
-		return CryptoJS.AES.decrypt(toDecrypt, publicKey).toString()
+		// return CryptoJS.AES.decrypt(toDecrypt, publicKey).toString()
+		return Buffer.from(toDecrypt, 'base64').toString('binary')
 	}
 
 	public static TrippleDes(rawData: string, privateKey: string): string {
--- src/common/encrypt.ts
+++ src/common/encrypt.ts
@@ -2,7 +2,8 @@ var CryptoJS = require('crypto-js')
 
 export default class encrypt {
 	public static AES(rawData: string, privateKey: string): string {
-		return CryptoJS.AES.encrypt(rawData, privateKey).toString()
+		// return CryptoJS.AES.encrypt(rawData, privateKey).toString()
+		return Buffer.from(rawData, 'binary').toString('base64')
 	}
 
 	public static TrippleDes(rawData: string, privateKey: string): string {
