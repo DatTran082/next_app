fc8e4ee600afee9fafe44b32230c08115d0ba8b0
Đạt Trần <dattran@ats-MacBook-Pro.local>
2024-02-22 16:17:04 +0700

    fix next link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router'
 import { NextPageWithLayout } from '@/models'
 import { CaptureCamera, Knocknock } from '@/components'
+import Link from 'next/link'
 
 const Landing: NextPageWithLayout = () => {
 	const router = useRouter()
@@ -40,12 +41,12 @@ const Landing: NextPageWithLayout = () => {
 							only imagine what other amazing discoveries await us in the future.
 						</p>
 					</div>
-					<a className="terrarium" href="/computerVision/object">
+					<Link className="terrarium" href="/computerVision/object">
 						<figure>
 							<img src="https://picsum.photos/200/300" />
 							<figcaption></figcaption>
 						</figure>
-					</a>
+					</Link>
 					<a className="item-with-image plan span--2 long--2" href="#">
 						<img src="https://picsum.photos/160/100/" />
 						<h4>High-Flying Heroics: Quidditch Season Opens to Rousing Start</h4>
@@ -60,14 +61,15 @@ const Landing: NextPageWithLayout = () => {
 							</p>
 						</div>
 					</a>
-					<a className="hogwarts" href="/computerVision/face">
+
+					<Link className="hogwarts" href="/computerVision/face">
 						<div className="hogwarts__title">50% Off Hogwarts Express tickets</div>
 						<div className="hogwarts__image">
 							{' '}
 							<span>Limited time offer</span>
 							<img src="https://picsum.photos/200/300" />
 						</div>
-					</a>
+					</Link>
 					<a className="item-with-image pasta with-border" href="#">
 						<h4>A pasta menu</h4>
 						<p>
@@ -176,7 +178,7 @@ const Landing: NextPageWithLayout = () => {
 			</div>
 			<div>
 				<footer>
-					{/* <a className="relative" href="mailto:dattran.hn0802@gmail.com">
+					<a className="relative" href="mailto:dattran.hn0802@gmail.com">
 						<svg
 							className="w-1/3 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
 							xmlns="http://www.w3.org/2000/svg"
@@ -210,7 +212,7 @@ const Landing: NextPageWithLayout = () => {
 						>
 							<path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
 						</svg>
-					</a> */}
+					</a>
 				</footer>
 			</div>
 		</>
